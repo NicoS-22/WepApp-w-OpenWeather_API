1725b6aec9c468e5f200c28c42ef8da94bf334a8
Nicolò Sartorelli MacOS <nicos@Air-di-Nicolo.homenet.telecomitalia.it>
2024-04-23 15:01:29 +0200

    Funzione non parte, 0 errori. Da rivedere

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
-const apiKey = "309e68a3adb7fa3e7115e61c39b922eb";
-        const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric";
-
-        const searchBox = document.querySelector("#searchBox");
-        const searchBtn = document.querySelector("#searchBtn");
-
+        const apiKey = "309e68a3adb7fa3e7115e61c39b922eb";
+        const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+        const searchBox = document.querySelector("#searchBox input");
+        const searchBtn = document.querySelector("#searchBtn button");
+        
+        
         async function checkWeather(city) {
             const result = await fetch(apiUrl + city + `&appid=${apiKey}`);
-
+            
             let data = await result.json();
-
-
+            
+            
             document.querySelector("#cityName").innerHTML = data.name;
             document.querySelector("#cityTemp").innerHTML = Math.round(data.main.temp) + "°c";
             document.querySelector("#cityHumidity").innerHTML = data.main.humidity + "%";
             document.querySelector("#cityWind").innerHTML = data.wind.speed + " Km/h";
-
+            
         }
-
+        
         searchBtn.addEventListener("click", ()=>{
             checkWeather(searchBox.value);
         })

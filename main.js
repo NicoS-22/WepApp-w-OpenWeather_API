e472765fb7210e15e9db47c370d21a5b5e394d01
Nicolò Sartorelli MacOS <nicos@MacBook-Air-di-Nicolo.local>
2024-04-25 12:30:09 +0200

    Risolto bug, app funzionante(aggiungere ev.Try&Catch, aggiungere stringa  quando input assegnato vuoto

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
         const apiKey = "309e68a3adb7fa3e7115e61c39b922eb";
         const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
-        const searchBox = document.querySelector("#searchBox input");
-        const searchBtn = document.querySelector("#searchBtn button");
+        const searchBox = document.querySelector("#searchBox");
+        const searchBtn = document.querySelector("#searchBtn");
         
         
         async function checkWeather(city) {
             const result = await fetch(apiUrl + city + `&appid=${apiKey}`);
-            
             let data = await result.json();
+            console.log(data)
             
             
             document.querySelector("#cityName").innerHTML = data.name;
@@ -17,6 +17,8 @@
             
         }
         
-        searchBtn.addEventListener("click", ()=>{
+        searchBtn.addEventListener("click", (event)=> {
+            event.preventDefault();
             checkWeather(searchBox.value);
-        })
+            /* console.log(searchBox.value); */
+        });
